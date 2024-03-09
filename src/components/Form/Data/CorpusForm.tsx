10863ae2ac9c9c5387c9d4a12ded9d6cccdd7c33
DavitMelikidze <davit.melikidze128@ens.tsu.ge>
2024-03-09 18:46:07 +0400

    cleanup of unnecessary console logs, removing redundant DataForm files, fixing minor merging issue

--- src/components/Form/Data/CorpusForm.tsx
+++ src/components/Form/Data/CorpusForm.tsx
@@ -16,7 +16,6 @@ const CorpusForm = ({setFetchedData, setLoading, setError, setTextRequest}: Corp
     const [amount, setAmount] = useState<Number>(10)
 
     useEffect(() => {
-        console.log("Here");
         if(setLoading)
             setLoading(true)
         if(setError)
--- src/store/context/playContext.tsx
+++ src/store/context/playContext.tsx
@@ -11,7 +11,6 @@ import { TextRequestFake } from '../../components/Form/Data/FakeWordsForm';
 import { TextRequestWord } from '../../components/Form/Data/CorpusForm';
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL
-console.log(SERVER_URL)
 
 interface ContextInterface extends PlayState, PlayActions {}
 
@@ -45,15 +44,14 @@ const PlayProvider: React.FunctionComponent<Props> = ({children} : Props) => {
     const StartListeners = () => {
         /** Messages */
         socket.on('user_connected', (users: string[]) => {
-            console.info('User connected message received');
+            //User connected message received
             PlayDispatch(updateUsers(users));
         });
 
         /** Messages */
         socket.on('user_disconnected', (params: {uid: string, matches: MatchStateList | undefined}) => {
-            console.info('User disconnected message received');
+            //User disconnected message received
             PlayDispatch(removeUser(params.uid));
-            console.log(params)
             if(params.matches)
                 PlayDispatch(updateMatches(params.matches))
         });
@@ -73,50 +71,44 @@ const PlayProvider: React.FunctionComponent<Props> = ({children} : Props) => {
         });
 
         socket.io.on('reconnect_failed', () => {
-            console.info('Reconnection failure.');
+            //Reconnection failure.
             alert('We are unable to connect you to the play service. Please make sure your internet connection is stable or try again later.');
         });
 
         socket.on('match_added', (matches: MatchStateList) => {
-            console.log("match_added event listened")
+            //match_added event listened
             PlayDispatch(updateMatches(matches))
         })
 
         socket.on('matches_modified', (matches: MatchStateList) => {            
-            console.log('matches have been modified')
+            //matches have been modified
             PlayDispatch(updateMatches(matches));
         })
 
         socket.on('match_modified', (matches: MatchStateList) => {
-            console.log("match has been modified");
-            console.log(matches)
+            //match has been modified
             PlayDispatch(updateMatches(matches));
         })
 
         socket.on('match_finished', (params: {matches: MatchStateList, match_id: string}) => {
-            console.log("match has finished");
-            console.log(params.matches)
+            //match has finished            
             PlayDispatch(updateMatches(params.matches))
             PlayDispatch(updateMatchId(params.match_id)) //match_id turns from uuid to mongoose.Types.ObjectId
             PlayDispatch(finishMatch(true))
         })
 
         socket.on("already_connected", () => {
-            console.log("already connected from a different tab")
+            //already connected from a different tab
             PlayDispatch(updateUsername("-1")); //Reserved username, lazy to add another boolean variable...
         })
 
-        socket.on("force_finish", () => {
-            console.log("forcing users to finish")
-            
-        })
     };
 
     const SendHandshake = async () => {
-        console.info('Sending handshake to server ...');
+        //Sending handshake to server ...        
 
         socket.emit('handshake', token, user?.username, async (username: string, uid: string, users: string[], match_id: string | undefined, matches:  MatchStateList) => {
-            console.info('User handshake callback message received');
+            //User handshake callback message received
             PlayDispatch(updateUsers(users));
             PlayDispatch(updateUID(uid));
             if(match_id)
@@ -129,13 +121,13 @@ const PlayProvider: React.FunctionComponent<Props> = ({children} : Props) => {
     };
 
     const CreateMatch = async (req: TextRequestFake | TextRequestWord, time_limit: number, user_limit: number) => {
-        console.info('trying to create a match');
+        //trying to create a match
         socket.emit('create_match',  
             req, 
             time_limit, 
             user_limit, 
             async (match_id: string, matches: MatchStateList) => {
-                console.info('the match has successfully been created! callback');
+                //the match has successfully been created! callback                
                 PlayDispatch(updateMatchId(match_id));
                 PlayDispatch(updateMatches(matches));
             }
@@ -143,30 +135,30 @@ const PlayProvider: React.FunctionComponent<Props> = ({children} : Props) => {
     }
 
     const JoinMatch = async(match_id: string) => {
-        console.info("joining the match");
+        //joining the match
         socket.emit('join_match', match_id, async () => {
-            PlayDispatch(updateMatchId(match_id))
-            console.log("match joined callback")
+            //match joined callback
+            PlayDispatch(updateMatchId(match_id))            
         });
     }
 
     const LeaveMatch = async(match_id: string) => {
-        console.info("leaving the match");
+        //leaving the match
         PlayDispatch(updateMatchId(undefined))
         if(PlayState.match_finished)
             PlayDispatch(finishMatch(false))
         socket.emit('leave_match', match_id, async () => {
-            console.info("left the match");            
+            //left the match
         })
     }
 
     const ModifyMatch = async(currentWordIndex: number) => {
-        console.log("notifying other players about current progress");
+        //notifying other players about current progress
         socket.emit('modify_match', PlayState.match_id, currentWordIndex, async ()=>{});
     }
 
     const NotifyFinish = async(user_wpm: number) => {
-        console.log("Notifying everyone about your finish");
+        //Notifying everyone about your finish
         socket.emit('user_finish', user_wpm, PlayState.match_id, async ()=>{});
     }
 
