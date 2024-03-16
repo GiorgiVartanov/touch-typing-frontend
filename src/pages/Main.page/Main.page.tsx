ad691e49b57dbc3cb35e575a99509dad50e18557
PhYell <94902895+PhYell@users.noreply.github.com>
2024-03-16 22:32:14 +0400

    changed main page design

--- src/pages/Main.page/Main.page.tsx
+++ src/pages/Main.page/Main.page.tsx
@@ -5,37 +5,69 @@ import getRandomMotivationalPhrase from "../../util/getRandomMotivationalPhrase"
 
 import "./styles.scss"
 
-import HeroKeyboard from "../../components/HeroKeyboard/HeroKeyboard"
-import TypingArea from "../../components/TypingArea/TypingArea"
+import MainPageText from "./MainPageText"
+import MainPageKeyboard from "./MainPageKeyboard"
 
 const MainPage = () => {
-  const [activeKeys, setActiveKeys] = useState<string[]>([])
-  const [showHeroKeyboard, setShowHeroKeyboard] = useState(window.innerWidth >= 1000)
+  const [showKeyboard, setShowKeyboard] = useState(window.innerWidth >= 1000)
 
-  const [text, setText] = useState<string>(getRandomMotivationalPhrase)
+  // const handleOnButtonHover = (buttonName: string) => {
+  //   setActiveKeys(buttonName.split(""))
+  // }
 
-  const handleOnButtonHover = (buttonName: string) => {
-    setActiveKeys(buttonName.split(""))
-  }
-  const handleOnMouseLeave = () => {
-    setActiveKeys([])
-  }
+  // const handleOnMouseLeave = () => {
+  //   setActiveKeys([])
+  // }
 
-  const renderKeyboard = () => {
-    if (!showHeroKeyboard) return
-
-    const changeText = () => {
-      setText((prevState) => getRandomMotivationalPhrase(prevState))
-    }
-
+  const renderOptions = () => {
     return (
-      <div>
-        <TypingArea
-          text={text}
-          className="typing-area-main-page"
-          handleTextFinish={changeText}
-        />
-        <HeroKeyboard activeKeys={activeKeys} />
+      <div className="options">
+        <Link
+          className="border shadow"
+          to="lessons"
+          // onMouseEnter={() => {
+          //   handleOnButtonHover("lessons")
+          // }}
+          // onMouseLeave={handleOnMouseLeave}
+        >
+          <p>Lessons</p>
+          <span>learn typing by completing tasks</span>
+        </Link>
+        <Link
+          to="play"
+          // onMouseEnter={() => {
+          //   handleOnButtonHover("online competition")
+          // }}
+          // onMouseLeave={handleOnMouseLeave}
+          className="border shadow"
+        >
+          <p>Competition</p>
+          <span>challenge other people</span>
+        </Link>
+        <div className="separate">
+          <Link
+            to="games"
+            // onMouseEnter={() => {
+            //   handleOnButtonHover("games")
+            // }}
+            // onMouseLeave={handleOnMouseLeave}
+            className="border shadow"
+          >
+            <p>Games</p>
+            <span>play games</span>
+          </Link>
+          <Link
+            to="practice"
+            // onMouseEnter={() => {
+            //   handleOnButtonHover("practice")
+            // }}
+            // onMouseLeave={handleOnMouseLeave}
+            className="border shadow"
+          >
+            <p>Practice</p>
+            <span>type texts</span>
+          </Link>
+        </div>
       </div>
     )
   }
@@ -43,7 +75,7 @@ const MainPage = () => {
   useEffect(() => {
     // updates the showHeroKeyboard state when the window is resized
     const handleResize = () => {
-      setShowHeroKeyboard(window.innerWidth >= 1000)
+      setShowKeyboard(window.innerWidth >= 1000)
     }
 
     // adds a listener for the window resize event
@@ -56,59 +88,14 @@ const MainPage = () => {
   }, [])
 
   return (
-    <div className="page main-page">
-      <div className="hero">
-        {renderKeyboard()}
-        <div className="options">
-          <Link
-            className="border shadow"
-            to="lessons"
-            onMouseEnter={() => {
-              handleOnButtonHover("lessons")
-            }}
-            onMouseLeave={handleOnMouseLeave}
-          >
-            <p>Lessons</p>
-            <span>learn typing by completing tasks</span>
-          </Link>
-          <Link
-            to="play"
-            onMouseEnter={() => {
-              handleOnButtonHover("online competition")
-            }}
-            onMouseLeave={handleOnMouseLeave}
-            className="border shadow"
-          >
-            <p>Competition</p>
-            <span>challenge other people</span>
-          </Link>
-          <div className="separate">
-            <Link
-              to="games"
-              onMouseEnter={() => {
-                handleOnButtonHover("games")
-              }}
-              onMouseLeave={handleOnMouseLeave}
-              className="border shadow"
-            >
-              <p>Games</p>
-              <span>play games</span>
-            </Link>
-            <Link
-              to="practice"
-              onMouseEnter={() => {
-                handleOnButtonHover("practice")
-              }}
-              onMouseLeave={handleOnMouseLeave}
-              className="border shadow"
-            >
-              <p>Practice</p>
-              <span>type texts</span>
-            </Link>
-          </div>
+    <div className="main-page">
+      <div className="main-content">
+        <div className="background-image"></div>
+        <div className="page">
+          {showKeyboard ? <MainPageText /> : null}
+          {renderOptions()}
         </div>
       </div>
-      <div className="page"></div>
     </div>
   )
 }
---
+++ src/pages/Main.page/MainPageText.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+import getRandomMotivationalPhrase from "../../util/getRandomMotivationalPhrase"
+
+import TypingArea from "../../components/TypingArea/TypingArea"
+
+const MainPageText = () => {
+  const [text, setText] = useState<string>(getRandomMotivationalPhrase)
+
+  const changeText = () => {
+    setText((prevState) => getRandomMotivationalPhrase(prevState))
+  }
+
+  return (
+    <TypingArea
+      text={text}
+      className="typing-area-main-page"
+      handleTextFinish={changeText}
+    />
+  )
+}
+export default MainPageText
